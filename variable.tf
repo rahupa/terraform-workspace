faa66b0eb69fa2cb01e8610520f415e71da2feb5
palrahul18298 <palrahul18298@gmail.com>
2022-05-04 11:53:28 +0530

    push

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "prefix" {
+  default = "Mrs"
+}
+
+variable "filename" {
+  default = "C:/Users/HP/Desktop/mohit.txt"
+}
+variable "content" {
+  default = "I Love You !"
+}
+
+variable "length" {
+  default = "1"
+}
+variable "separator" {
+  default = "."
+}
